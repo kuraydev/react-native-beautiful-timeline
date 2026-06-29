b83e39131436c962d071394d9994b8b85f1c0583
kuraydev <kuraydev.io@gmail.com>
2026-06-29 16:27:25 +0300

    fix(build): remove duplicate lib/index.ts (keep canonical index.tsx)

--- lib/index.ts
+++
@@ -1,6 +0,0 @@
-import Timeline from "./Timeline";
-
-export default Timeline;
-export { Timeline };
-export type { TimelineProps } from "./Timeline";
-export type { ITimeline, ITimelineData } from "./models";
